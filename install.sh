670fe45be70cfa7a4e8fcbfc336cc93104c2ecbd
Brett Epps <brett.epps@software.dell.com>
2015-11-28 14:40:13 -0600

    Add install script that creates symlinks

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+ln -sf $HOME/.dotfiles/.gitconfig .gitconfig
+ln -sf $HOME/.dotfiles/.zpreztorc .zpreztorc
+ln -sf $HOME/.dotfiles/.zprofile .zprofile
+ln -sf $HOME/.dotfiles/.zshenv .zshenv
+ln -sf $HOME/.dotfiles/.zshrc .zshrc
