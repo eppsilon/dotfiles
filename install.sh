e2625973ecb8a636f78155b9ad1a9022eddad0c9
Brett Epps <brett.epps@quest.com>
2022-07-14 14:50:51 -0700

    add node-cli and update install script

--- install.sh
+++ install.sh
@@ -6,19 +6,26 @@ DOTFILES=(".gitconfig"
           ".zprofile"
           ".zshenv"
           ".zshrc"
+          ".node-cli/.envrc"
+          ".node-cli/.nvmrc"
+          ".node-cli/package.json"
+          ".node-cli/yarn.lock"
           "Library/Application Support/Beyond Compare/BCColors.xml"
           "Library/Application Support/Beyond Compare/BCFileFormats.xml"
           "Library/Application Support/Beyond Compare/BCPreferences.xml"
           "Library/Application Support/Code/User/settings.json")
 
-DOTDIRS=()
+DOTDIRS=(".node-cli")
 
 for dotdir in "${DOTDIRS[@]}"
 do
-  mkdir -p "$dotdir"
+  mkdir -p "$HOME/$dotdir"
 done
 
 for dotfile in "${DOTFILES[@]}"
 do
+  if [[ "${dotfile:0:1}" == '.' ]]; then
+    dotfile = ${dotfile:1}
+  fi
   ln -sf "$HOME/.dotfiles/$dotfile" "$HOME/$dotfile"
 done
